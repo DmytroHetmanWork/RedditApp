53387725a4c32d3e92e916f1c9b392b75ea3160d
Vladyslav Kozyr <vladyslav.kozyr@gmail.com>
2024-03-28 20:53:09 +0200

    feat: login & comments

--- RedditRead/SwiftUIViews/CommentsList.swift
+++ RedditRead/SwiftUIViews/CommentsList.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CommentsList: View {
     @EnvironmentObject var viewModel: CommentsVM
-    
+
     var body: some View {
         NavigationView {
             ScrollView {
@@ -27,11 +27,38 @@ struct CommentsList: View {
             .navigationBarTitleDisplayMode(.inline)
         }
         .navigationViewStyle(.stack)
+        Divider()
+        addComment
     }
-    
+
     private var terminatorV: some View {
         Color.gray
             .frame(height: 2)
             .padding(.vertical, 10)
+    }
+
+    private var addComment: some View {
+        HStack {
+            TextField("Add your comment", text: $viewModel.newComment)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .lineLimit(nil)
+                .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 10))
+                .shadow(color: Color.black.opacity(0.1), radius: 2)
+
+            Button(action: {
+                viewModel.postComment()
+            }) {
+                Image(systemName: "plus")
+                    .frame(width: 22, height: 22)
+                    .foregroundColor(.black)
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(12)
+            .shadow(color: Color.black.opacity(0.2), radius: 2)
+        }
+        .padding(EdgeInsets(top: 0, leading: 10, bottom: 8, trailing: 16))
+        .background(Color.white)
+        .cornerRadius(10)
     }
 }
